72c6300baa7835191ac20aa19f76757bbc7ab7c7
Lakshman <lakshman@moiiai.com>
2024-03-12 16:41:11 +0100

    Updates

---
+++ precache-manifest.8a5651e69dd6016bd1a9cf2ea7f98e89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c22f687916e84824fb70",
+    "url": "/Embedding_Explorer/static/css/main.1b300477.chunk.css"
+  },
+  {
+    "revision": "c22f687916e84824fb70",
+    "url": "/Embedding_Explorer/static/js/main.c22f6879.chunk.js"
+  },
+  {
+    "revision": "d41fcd46c642c4af341e",
+    "url": "/Embedding_Explorer/static/js/1.d41fcd46.chunk.js"
+  },
+  {
+    "revision": "5e921f02fbd56c0b30af",
+    "url": "/Embedding_Explorer/static/js/runtime~main.5e921f02.js"
+  },
+  {
+    "revision": "a079f984a1eecaa00d7ca6634fb5113e",
+    "url": "/Embedding_Explorer/index.html"
+  }
+];
